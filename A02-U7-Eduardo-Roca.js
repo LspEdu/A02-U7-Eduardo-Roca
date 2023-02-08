a3e5edd69d17f15cc9c4855df68aa5728d081536
Eduardo <eduardolmsan@gmail.com>
2023-02-08 16:21:34 +0100

    Formulario con boton

--- A02-U7-Eduardo-Roca.js
+++ A02-U7-Eduardo-Roca.js
@@ -1,10 +1,20 @@
 const HTTP = new XMLHttpRequest,
-      url = "A02-U7-Eduardo-Roca.php";
+      url = "A02-U7-Eduardo-Roca.json";
 
 HTTP.open("GET", url);
 HTTP.send();
 
+
 HTTP.onreadystatechange = (e) => {
-    console.log(HTTP.responseText);
+console.log(HTTP.responseText);
 }
 
+document.querySelector('input[type="text"]').addEventListener('keydown', () => {
+    document.forms[0].submit;
+})
+
+document.forms[0].addEventListener('submit', () => {
+        HTTP.onreadystatechange = (e) => {
+            console.log(HTTP.responseText);
+        }
+})
