efc0fe18e320fbd668ae1e3c6c68c2ddbf409a39
Miguel Ángel Roca González <miguelangel.roca@iesdonana.org>
2023-02-10 16:20:51 +0100

    Funcionando

--- A02-U7-Eduardo-Roca.js
+++ A02-U7-Eduardo-Roca.js
@@ -1,20 +1,15 @@
-const HTTP = new XMLHttpRequest,
-      url = "A02-U7-Eduardo-Roca.json";
-
-HTTP.open("GET", url);
-HTTP.send();
-
-
-HTTP.onreadystatechange = (e) => {
-console.log(HTTP.responseText);
+function show(str) {
+  if (str.length == 0) {
+    document.getElementById("suggestion").innerHTML = "";
+    return;
+  } else {
+    var http = new XMLHttpRequest();
+    http.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("suggestion").innerHTML = this.responseText;
+      }
+    };
+    http.open("GET", "A02-U7-Eduardo-Roca.php?city=" + str, true);
+    http.send();
+  }
 }
-
-document.querySelector('input[type="text"]').addEventListener('keydown', () => {
-    document.forms[0].submit;
-})
-
-document.forms[0].addEventListener('submit', () => {
-        HTTP.onreadystatechange = (e) => {
-            console.log(HTTP.responseText);
-        }
-})
--- A02-U7-Eduardo-Roca.php
+++ A02-U7-Eduardo-Roca.php
@@ -1,3 +1,26 @@
 <?php
+// Array con las sugerencias.
+$cities[] = "Madrid";
 
-    
+// Obtiene la petición cuyo parametro es "city".
+$city = $_REQUEST["city"];
+
+$found = "";
+
+// Bucle que comprueba que el parametro no sea cadena vacia.
+if ($city !== "") {
+  $city = strtolower($city); // Convierte la cadena en minuscula.
+  $len = strlen($city); // Guarda el tamaño de la cadena.
+  foreach ($cities as $cit) {
+    if (stristr($city, substr($cit, 0, $len))) { // Compara si el caracter escrito se encuentra en la cadena para mostrar las sugerencias.
+      if ($found === "") {
+        $found = $cit;
+      } else {
+        $found .= ", $cit";
+      }
+    }
+  }
+}
+
+// Muestra la sugerencia o "No existe sugerencia" en caso de ser cadena vacia.
+echo $found === "" ? "No existe sugerencia..." : $found;
